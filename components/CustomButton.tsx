9e50849bf3de4356370ecf9a1d6c3b1e5f062a07
nishitpatel1292 <73834549+nishitpatel1292@users.noreply.github.com>
2023-09-28 13:48:45 +0530

    custombutton

---
+++ components/CustomButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { CustomButtonProps } from '@/types';
+import React from 'react'
+
+const CustomButton = ({buttonText, containerStyles, buttonMethod}:CustomButtonProps) => {
+  return (
+    <button 
+    disabled={false}
+    type='button'
+    className={`custom-btn ${containerStyles}`}
+    onClick={buttonMethod}>
+        {buttonText}
+    </button>
+  )
+}
+
+export default CustomButton
